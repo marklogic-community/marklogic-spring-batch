80ea0e7a282ad9a0850350077b3024b229ed3543
Scott Stafford <sas4z@yahoo.com>
2016-01-19 23:16:31 -0500

    Exclude out Spring BasicConfig class

--- src/main/java/com/marklogic/client/spring/batch/Application.java
+++ src/main/java/com/marklogic/client/spring/batch/Application.java
@@ -6,10 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = { "com.marklogic.spring.batch" })
+@ComponentScan(
+	basePackageClasses = { com.marklogic.client.spring.batch.SpringBatchConfig.class, com.marklogic.client.spring.DatabaseConfig.class },
+	excludeFilters = @ComponentScan.Filter(value = com.marklogic.client.spring.BasicConfig.class, type = FilterType.ASSIGNABLE_TYPE)
+)
 public class Application {
 	
 	private static Log log = LogFactory.getLog(Application.class);
--- src/main/java/com/marklogic/client/spring/batch/writer/DocumentItemWriter.java
+++ src/main/java/com/marklogic/client/spring/batch/writer/DocumentItemWriter.java
@@ -3,27 +3,23 @@ package com.marklogic.client.spring.batch.writer;
 import java.util.List;
 
 import com.marklogic.client.DatabaseClient;
-import com.marklogic.client.DatabaseClientFactory;
-import com.marklogic.client.DatabaseClientFactory.Authentication;
 import com.marklogic.client.document.XMLDocumentManager;
+import com.marklogic.client.helper.DatabaseClientProvider;
 import com.marklogic.client.io.DOMHandle;
 
 import org.w3c.dom.Document;
 
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 
 public class DocumentItemWriter implements ItemWriter<Document> {
 
 	@Autowired
-	private Environment env;
+	DatabaseClientProvider databaseClientProvider;
 	
 	@Override
 	public void write(List<? extends Document> items) throws Exception {
-		DatabaseClient client = DatabaseClientFactory.newClient(
-				   env.getProperty("marklogic.host"), Integer.parseInt(env.getProperty("marklogic.port")), 
-				   env.getProperty("marklogic.user"), env.getProperty("marklogic.password"), Authentication.DIGEST);
+		DatabaseClient client = databaseClientProvider.getDatabaseClient();
 		XMLDocumentManager docMgr = client.newXMLDocumentManager();
 		for (int i = 0; i < items.size(); i++) {
 			Document doc = items.get(i);
