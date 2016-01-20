227579bf7ae73df1a1d42b94a4b2bf97f24fd6c6
Scott Stafford <sas4z@yahoo.com>
2016-01-19 22:19:30 -0500

    Add DatabaseConfig class

---
+++ src/main/java/com/marklogic/client/spring/DatabaseConfig.java
@@ -0,0 +1,53 @@
+package com.marklogic.client.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.marklogic.client.helper.DatabaseClientConfig;
+import com.marklogic.client.helper.DatabaseClientProvider;
+import com.marklogic.xcc.template.XccTemplate;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Autowired
+	private Environment env;
+
+	protected String getHost() {
+		return env.getProperty("marklogic.host", "localhost");
+	}
+	
+	protected int getPort() {
+		return Integer.parseInt(env.getProperty("marklogic.port", "8000"));
+	}
+	
+	protected String getUser() {
+		return env.getProperty("marklogic.user", "admin");
+	}
+	
+	protected String getPassword() {
+		return env.getProperty("marklogic.password", "admin");
+	}
+	
+	protected String getDatabaseName() {
+		return env.getProperty("marklogic.database", "Documents");
+	}
+
+    @Bean
+    public DatabaseClientConfig databaseClientConfig() {
+        return new DatabaseClientConfig(getHost(), getPort(), getUser(), getPassword());
+    }
+
+    @Bean
+    public XccTemplate xccTemplate() {
+        return new XccTemplate(String.format("xcc://%s:%s@%s:8000/%s", getUser(), getPassword(), getHost(), getDatabaseName()));
+    }
+
+    @Bean
+    public DatabaseClientProvider databaseClientProvider() {
+        return new SimpleDatabaseClientProvider();
+    }
+
+}
