316bef056a3615509e833e977c528641eafa3713
Scott Stafford <sas4z@yahoo.com>
2016-01-25 19:12:38 -0500

    Check in ml-gradle configuration for spring-batch database

--- src/main/java/org/springframework/batch/core/package-info.java
+++ src/main/java/org/springframework/batch/core/package-info.java
@@ -1,9 +1,8 @@
 @XmlSchema(
 	namespace="http://marklogic.com/spring-batch",
-	elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-	
+	elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED,	
 	xmlns = {
-			@javax.xml.bind.annotation.XmlNs(prefix = "sb", namespaceURI="http://marklogic.com/spring-batch")
+			@javax.xml.bind.annotation.XmlNs(prefix = "sb", namespaceURI="http://projects.spring.io/spring-batch/")
 	}
 )
 
