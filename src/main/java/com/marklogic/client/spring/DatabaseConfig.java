07d5ebcdceea0fc19f0df67179d55978e5e86b8a
Rob Rudin <rjrudin@gmail.com>
2016-01-29 09:49:49 -0500

    Reader/writer for deleting URIs

--- src/main/java/com/marklogic/client/spring/DatabaseConfig.java
+++ src/main/java/com/marklogic/client/spring/DatabaseConfig.java
@@ -9,36 +9,31 @@ import org.springframework.core.env.Environment;
 
 import com.marklogic.client.helper.DatabaseClientConfig;
 import com.marklogic.client.helper.DatabaseClientProvider;
-import com.marklogic.xcc.template.XccTemplate;
 
 @Configuration
 @PropertySource("config/application.properties")
 public class DatabaseConfig {
-	
-	@Autowired
-	private Environment env;
 
-	protected String getHost() {
-		return env.getProperty("marklogic.host", "localhost");
-	}
-	
-	protected int getPort() {
-		return Integer.parseInt(env.getProperty("marklogic.port", "8000"));
-	}
-	
-	protected String getUser() {
-		return env.getProperty("marklogic.user", "admin");
-	}
-	
-	protected String getPassword() {
-		return env.getProperty("marklogic.password", "admin");
-	}
-	
-	protected String getDatabaseName() {
-		return env.getProperty("marklogic.database", "Documents");
-	}
-	
-	@Bean
+    @Autowired
+    private Environment env;
+
+    protected String getHost() {
+        return env.getProperty("marklogic.host", "localhost");
+    }
+
+    protected int getPort() {
+        return Integer.parseInt(env.getProperty("marklogic.port", "8000"));
+    }
+
+    protected String getUser() {
+        return env.getProperty("marklogic.user", "admin");
+    }
+
+    protected String getPassword() {
+        return env.getProperty("marklogic.password", "admin");
+    }
+
+    @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
         PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
         c.setIgnoreResourceNotFound(true);
@@ -48,11 +43,6 @@ public class DatabaseConfig {
     @Bean
     public DatabaseClientConfig databaseClientConfig() {
         return new DatabaseClientConfig(getHost(), getPort(), getUser(), getPassword());
-    }
-
-    @Bean
-    public XccTemplate xccTemplate() {
-        return new XccTemplate(String.format("xcc://%s:%s@%s:8000/%s", getUser(), getPassword(), getHost(), getDatabaseName()));
     }
 
     @Bean
