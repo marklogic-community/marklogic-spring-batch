f87b7986adf82b60a629a2b7f302a1d1238f3c55
Rob Rudin <rjrudin@gmail.com>
2016-02-03 09:24:01 -0500

    #9 All tests using BasicTestConfig now

--- src/test/java/com/marklogic/client/spring/batch/geonames/MarshallGeonameObjectToXmlTest.java
+++ src/test/java/com/marklogic/client/spring/batch/geonames/MarshallGeonameObjectToXmlTest.java
@@ -4,44 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.geonames.Geoname;
+import org.jdom2.input.DOMBuilder;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Document;
 
-import com.marklogic.client.spring.batch.AbstractSpringBatchTest;
+import com.marklogic.client.spring.batch.AbstractBatchTest;
 import com.marklogic.junit.Fragment;
 
-import org.jdom2.input.DOMBuilder;
+public class MarshallGeonameObjectToXmlTest extends AbstractBatchTest {
 
-public class MarshallGeonameObjectToXmlTest extends AbstractSpringBatchTest {
-	
-	@Autowired
-	DocumentBuilder documentBuilder;
-	
-	@Autowired
-	DOMBuilder domBuilder;
-	
-	@Test
-	public void marshallGeonameTest() throws JAXBException {
-		Geoname geo = new Geoname();
-		geo.setId("123");
-		List<String> names = new ArrayList<String>();
-		names.add("Alpha");
-		names.add("Beta");
-		geo.setNames(names);
-		
-		Document w3cDoc = documentBuilder.newDocument();
-		Marshaller marshaller = JAXBContext.newInstance(org.geonames.Geoname.class).createMarshaller();
-		marshaller.marshal(geo, w3cDoc);
-		Fragment frag = new Fragment(domBuilder.build(w3cDoc));
-		frag.setNamespaces(getNamespaceProvider().getNamespaces());
-		System.out.println(frag.getPrettyXml());
-		frag.assertElementExists("/geo:geoname/geo:names/geo:name[2][text() = 'Beta']");	
-	}
+    @Test
+    public void marshallGeonameTest() throws Exception {
+        Geoname geo = new Geoname();
+        geo.setId("123");
+        List<String> names = new ArrayList<String>();
+        names.add("Alpha");
+        names.add("Beta");
+        geo.setNames(names);
+
+        Document w3cDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Marshaller marshaller = JAXBContext.newInstance(org.geonames.Geoname.class).createMarshaller();
+        marshaller.marshal(geo, w3cDoc);
+        Fragment frag = new Fragment(new DOMBuilder().build(w3cDoc));
+        frag.setNamespaces(getNamespaceProvider().getNamespaces());
+        frag.prettyPrint();
+        frag.assertElementExists("/geo:geoname/geo:names/geo:name[2][text() = 'Beta']");
+    }
 
 }
