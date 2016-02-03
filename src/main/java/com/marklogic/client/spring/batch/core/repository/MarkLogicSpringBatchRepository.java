8464be912c3821949d615e3dc1c7a20c7295c4e7
Rob Rudin <rjrudin@gmail.com>
2016-02-03 09:48:05 -0500

    #6 Repackaged all of the tests

--- src/main/java/com/marklogic/client/spring/batch/core/repository/MarkLogicSpringBatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.marklogic.client.spring.batch.core.repository;
-
-public interface MarkLogicSpringBatchRepository {
-
-	public final String SPRING_BATCH_DIR = "/projects.spring.io/spring-batch/";
-	public final String COLLECTION_JOB_EXECUTION = "http://marklogic.com/spring-batch/job-execution";
-	public final String COLLETION_JOB_INSTANCE =  "http://marklogic.com/spring-batch/job-instance";
-}
---
+++ src/main/java/com/marklogic/spring/batch/corb/CorbReader.java
@@ -0,0 +1,54 @@
+package com.marklogic.spring.batch.corb;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.eval.EvalResult;
+import com.marklogic.client.eval.EvalResultIterator;
+import com.marklogic.spring.batch.item.AbstractItemStreamReader;
+
+public class CorbReader<T> extends AbstractItemStreamReader<T> {
+
+    private String urisModule;
+    private DatabaseClient client;
+
+    private EvalResultIterator resultIterator;
+
+    public CorbReader(DatabaseClient client, String urisModule) {
+        this.client = client;
+        this.urisModule = urisModule;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        this.resultIterator = client.newServerEval().modulePath(urisModule).eval();
+    }
+
+    @Override
+    @SuppressWarnings({ "unchecked" })
+    public T read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        T result = null;
+        if (resultIterator.hasNext()) {
+            EvalResult item = resultIterator.next();
+            switch (item.getType()) {
+            case INTEGER: {
+                result = (T) item.getAs(Integer.class);
+                break;
+            }
+            case STRING: {
+                result = (T) item.getAs(String.class);
+                break;
+            }
+            default: {
+                break;
+            }
+            }
+        }
+        return result;
+    }
+
+}
