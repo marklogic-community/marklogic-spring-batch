3acd2aae00bb9a3f845270f7b57d31f664f2beeb
Scott Stafford <sas4z@yahoo.com>
2016-02-11 12:33:45 -0500

    Add marshall jobInstance unit test

--- src/main/java/com/marklogic/spring/batch/core/repository/MarkLogicSpringBatchRepository.java
+++ src/main/java/com/marklogic/spring/batch/core/repository/MarkLogicSpringBatchRepository.java
@@ -5,4 +5,5 @@ public interface MarkLogicSpringBatchRepository {
 	public final String SPRING_BATCH_DIR = "/projects.spring.io/spring-batch";
 	public final String COLLECTION_JOB_EXECUTION = "http://marklogic.com/spring-batch/job-execution";
 	public final String COLLETION_JOB_INSTANCE =  "http://marklogic.com/spring-batch/job-instance";
+	public final String NAMESPACE = "http://projects.spring.io/spring-batch";
 }
--- src/test/java/com/marklogic/spring/batch/core/repository/MarshallJobInstanceToXmlTest.java
+++ src/test/java/com/marklogic/spring/batch/core/repository/MarshallJobInstanceToXmlTest.java
@@ -3,12 +3,16 @@ package com.marklogic.spring.batch.core.repository;
 import java.util.Date;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.jdom2.input.DOMBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Document;
@@ -24,22 +28,27 @@ public class MarshallJobInstanceToXmlTest extends AbstractSpringBatchTest {
 	
 	XmlJobParameters jobParams;
 	
+	Document doc;
+	Marshaller marshaller;
+	
 	@Before
-	public void setup() {
+	public void setup() throws Exception {
 		jobParametersBuilder.addString("stringTest", "Joe Cool", true);
 		jobParametersBuilder.addDate("start", new Date(), false);
 		jobParametersBuilder.addLong("longTest", 1239L, false);
 		jobParametersBuilder.addDouble("doubleTest", 1.35D, false);
 		jobParams = new XmlJobParameters(jobParametersBuilder.toJobParameters());
+		
+		doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		
+		marshaller = JAXBContext.newInstance(XmlJobParameters.class, JobInstance.class).createMarshaller();
+	    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 	}
 	
 	@Test
     public void marshallJobParametersTest() throws Exception {
-        Document w3cDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        Marshaller marshaller = JAXBContext.newInstance(XmlJobParameters.class).createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(jobParams, w3cDoc);
-        Fragment frag = new Fragment(new DOMBuilder().build(w3cDoc));
+        marshaller.marshal(jobParams, doc);
+        Fragment frag = new Fragment(new DOMBuilder().build(doc));
         frag.setNamespaces(getNamespaceProvider().getNamespaces()); 
         frag.assertElementExists("/sb:jobParameters/sb:jobParameter[@key = 'stringTest' and text() = 'Joe Cool' and @identifier = 'true']");
         frag.assertElementExists("/sb:jobParameters/sb:jobParameter[@key = 'longTest' and text() = '1239' and @identifier = 'false']");
@@ -47,4 +56,15 @@ public class MarshallJobInstanceToXmlTest extends AbstractSpringBatchTest {
         frag.assertElementExists("/sb:jobParameters/sb:jobParameter[@key = 'doubleTest' and text() = '1.35' and @identifier = 'false']");
         frag.prettyPrint();
     }
+	
+	@Test
+	public void marshallJobInstanceTest() throws Exception {
+		JobInstance jobInstance = new JobInstance(123L, "TestJobInstance");
+		JAXBElement<JobInstance> element = new JAXBElement<JobInstance>(new QName(MarkLogicSpringBatchRepository.NAMESPACE, "jobInstance"), JobInstance.class, jobInstance);
+		marshaller.marshal(element, doc);
+        Fragment frag = new Fragment(new DOMBuilder().build(doc));
+        frag.setNamespaces(getNamespaceProvider().getNamespaces()); 
+        frag.prettyPrint();
+        frag.assertElementValue("/sb:jobInstance/sb:id", "123");
+	}
 }
