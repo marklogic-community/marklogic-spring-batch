584c80b8f90793e6176e858cc3fb132133c6da16
Rob Rudin <rjrudin@gmail.com>
2016-02-12 15:16:59 -0500

    #13 No longer needs context config

--- src/test/java/com/marklogic/spring/batch/sql/AbstractHsqlTest.java
+++ src/test/java/com/marklogic/spring/batch/sql/AbstractHsqlTest.java
@@ -4,15 +4,12 @@ import org.junit.After;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.test.context.ContextConfiguration;
 
 import com.marklogic.spring.batch.AbstractSpringBatchTest;
-import com.marklogic.spring.batch.TestConfig;
 
 /**
  * Provides some support for tests that need to stand up an embedded HSQL database.
  */
-@ContextConfiguration(classes = { TestConfig.class })
 public abstract class AbstractHsqlTest extends AbstractSpringBatchTest {
 
     protected EmbeddedDatabase db;
