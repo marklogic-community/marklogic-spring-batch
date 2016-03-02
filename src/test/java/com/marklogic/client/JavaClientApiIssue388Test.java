a251a76223b76cd5036cd0f12a7769ff3cfbf5e4
Scott Stafford <sas4z@yahoo.com>
2016-03-02 06:09:32 -0500

    Add a package to this unit test

--- src/test/java/com/marklogic/client/JavaClientApiIssue388Test.java
+++ src/test/java/com/marklogic/client/JavaClientApiIssue388Test.java
@@ -1,9 +1,16 @@
+package com.marklogic.client;
+
 import org.junit.Test;
 
 import com.marklogic.client.query.StructuredQueryBuilder;
 import com.marklogic.client.query.StructuredQueryDefinition;
 import com.marklogic.junit.Fragment;
 import com.marklogic.spring.batch.AbstractSpringBatchTest;
+
+/*
+ * Used to test Java Client Api Issue 388
+ * https://github.com/marklogic/java-client-api/issues/388
+ */
 
 public class JavaClientApiIssue388Test extends AbstractSpringBatchTest {
 	
