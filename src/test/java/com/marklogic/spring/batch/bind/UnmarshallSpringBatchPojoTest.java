c2f2c4e59ab486dfbffda43c8b7b9a56fa1243cd
Scott Stafford <sas4z@yahoo.com>
2016-03-10 15:52:18 -0500

    Submit failing unmarshal job execution test

--- src/test/java/com/marklogic/spring/batch/bind/UnmarshallSpringBatchPojoTest.java
+++ src/test/java/com/marklogic/spring/batch/bind/UnmarshallSpringBatchPojoTest.java
@@ -68,24 +68,8 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	
 	@Test
 	public void unmarshallJobExecution() throws Exception {
-		StringReader xml = new StringReader("<msb:jobExecution xmlns:msb=\"http://marklogic.com/spring-batch\">" +
-				"<msb:id>12345</msb:id>" +
-			    "<msb:createDateTime>2016-02-16T09:37:14.263-05:00</msb:createDateTime>" +
-			    "<msb:exitCode>exitCode=UNKNOWN;exitDescription=</msb:exitCode>" +
-			    "<msb:jobInstance>" +
-			        "<msb:id>123</msb:id>" +
-			        "<msb:jobName>TestJobInstance</msb:jobName>" +
-			    "</msb:jobInstance>" +
-			    "<msb:jobParameters>" +
-			        "<msb:jobParameter key=\"stringTest\" type=\"STRING\" identifier=\"true\">Joe Cool</msb:jobParameter>" +
-			        "<msb:jobParameter key=\"start\" type=\"DATE\" identifier=\"false\">2016-02-16T09:37:14-0500</msb:jobParameter>" +
-			        "<msb:jobParameter key=\"longTest\" type=\"LONG\" identifier=\"false\">1239</msb:jobParameter>" +
-			        "<msb:jobParameter key=\"doubleTest\" type=\"DOUBLE\" identifier=\"false\">1.35</msb:jobParameter>" +
-			    "</msb:jobParameters>" +
-			    "<msb:status>STARTING</msb:status>" +
-			    "<msb:stepExecutions/>" +
-			"</msb:jobExecution>");
-		AdaptedJobExecution jobExecution = (AdaptedJobExecution)unmarshaller.unmarshal(xml);
+		Resource jobExecutionXml = ctx.getResource("classpath:/xml/job-execution.xml");
+		AdaptedJobExecution jobExecution = (AdaptedJobExecution)unmarshaller.unmarshal(jobExecutionXml.getInputStream());
 		assertEquals(MarkLogicSpringBatch.SPRING_BATCH_DIR + "12345.xml", jobExecution.getUri());
 		assertEquals(Long.valueOf(12345L), jobExecution.getId());
 		
