8773208241d9cd870e2536f7a968d97b8d9b8ccd
Scott Stafford <sas4z@yahoo.com>
2016-03-12 18:17:03 -0500

    Return jobExecutions in descending order by createDateTime

--- src/test/java/com/marklogic/spring/batch/core/explore/GetJobExecutionsFromJobExplorerTest.java
+++ src/test/java/com/marklogic/spring/batch/core/explore/GetJobExecutionsFromJobExplorerTest.java
@@ -3,10 +3,15 @@ package com.marklogic.spring.batch.core.explore;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -16,7 +21,6 @@ import com.marklogic.spring.batch.JobParametersTestUtils;
 @ActiveProfiles(profiles = "marklogic", inheritProfiles = false)
 public class GetJobExecutionsFromJobExplorerTest extends AbstractSpringBatchTest {
 
-	
 	@Autowired
 	private JobExplorer jobExplorer;
 	
@@ -36,6 +40,26 @@ public class GetJobExecutionsFromJobExplorerTest extends AbstractSpringBatchTest
 		JobExecution jobExecution = jobRepository.createJobExecution("sampleJob", JobParametersTestUtils.getJobParameters());
 		JobExecution persistedJobExecution = jobExplorer.getJobExecution(jobExecution.getId());
 		assertEquals(persistedJobExecution.getId(), jobExecution.getId());
+	}
+	
+	@Test
+	public void ReturnJobExecutionsAreDescendingOrderTest() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+		JobInstance jobInstance = new JobInstance(123L, "firstJob");
+		JobExecution jobExecution1 = jobRepository.createJobExecution(jobInstance, JobParametersTestUtils.getJobParameters(), null);
+		jobExecution1.setStatus(BatchStatus.FAILED);
+		
+		JobParametersBuilder builder = new JobParametersBuilder();
+		builder.addLong("test", 12345L, true);
+		JobExecution jobExecution2 = jobRepository.createJobExecution(jobInstance, builder.toJobParameters(), null);
+		
+		builder.addLong("test2", 12346L, true);
+		JobExecution jobExecution3 = jobRepository.createJobExecution(jobInstance, builder.toJobParameters(), null);
+		
+		List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+		assertNotNull(jobExecutions);
+		assertEquals(jobExecution3.getId(), jobExecutions.get(0).getId());
+		assertEquals(jobExecution2.getId(), jobExecutions.get(1).getId());
+		assertEquals(jobExecution1.getId(), jobExecutions.get(2).getId());	
 	}
 		
 }
