b1f9a356f441ec0d6919ec645865bf764eef1a87
Scott Stafford <sas4z@yahoo.com>
2016-03-12 20:27:39 -0500

    Add JobParameters incrementer

--- src/test/java/com/marklogic/spring/batch/JobParametersTestUtils.java
+++ src/test/java/com/marklogic/spring/batch/JobParametersTestUtils.java
@@ -4,16 +4,33 @@ import java.util.Date;
 
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
 
-public class JobParametersTestUtils {
+public class JobParametersTestUtils implements JobParametersIncrementer {
+	
+	private static JobParameters jobParameters = new JobParametersBuilder().toJobParameters();
 	
 	public static JobParameters getJobParameters() {
 		JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
-		jobParametersBuilder.addString("stringTest", "Joe Cool", true);
-		jobParametersBuilder.addDate("start", new Date(), false);
-		jobParametersBuilder.addLong("longTest", 1239L, false);
-		jobParametersBuilder.addDouble("doubleTest", 1.35D, false);
-		return jobParametersBuilder.toJobParameters();
+		if (jobParameters.isEmpty()) {
+			jobParametersBuilder.addLong("id", 1L, true);
+			jobParametersBuilder.addString("stringTest", "Joe Cool", true);
+			jobParametersBuilder.addDate("start", new Date(), false);
+			jobParametersBuilder.addLong("longTest", 1239L, false);
+			jobParametersBuilder.addDouble("doubleTest", 1.35D, false);
+			jobParameters = jobParametersBuilder.toJobParameters();
+		} else {
+			JobParametersTestUtils utils = new JobParametersTestUtils();
+			jobParameters = utils.getNext(jobParameters);
+		}
+		return jobParameters;
 		
+	}
+
+	@Override
+	public JobParameters getNext(JobParameters parameters) {
+		JobParametersBuilder builder = new JobParametersBuilder(parameters);
+		builder.addLong("id", parameters.getLong("id") + 1);
+		return builder.toJobParameters();
 	}
 }
