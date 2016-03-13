5ed5f1d984359706d6ac052f118c339d6001e128
Scott Stafford <sas4z@yahoo.com>
2016-03-12 21:12:41 -0500

    Adding tests for get job instance count

--- src/test/java/com/marklogic/spring/batch/core/explore/GetJobInstancesTest.java
+++ src/test/java/com/marklogic/spring/batch/core/explore/GetJobInstancesTest.java
@@ -3,6 +3,8 @@ package com.marklogic.spring.batch.core.explore;
 import org.junit.Test;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,18 +23,23 @@ public class GetJobInstancesTest extends AbstractSpringBatchTest {
 	
 	private final String JOB_NAME = "testJob";
 	
-	/*
-	 * Given a JobInstance
-	 * When the job instance is retried by ID
-	 * Then 
-	 */
 	@Test
-	public void retrieveJobInstanceById() {
+	public void retrieveJobInstanceByIdTest() {
 		JobInstance expectedJobInstance = jobRepository.createJobInstance(JOB_NAME, JobParametersTestUtils.getJobParameters());
 		JobInstance actualJobInstance = jobExplorer.getJobInstance(expectedJobInstance.getId());
 		assertTrue(expectedJobInstance.equals(actualJobInstance));
 	}
 	
+	@Test
+	public void getJobInstanceCountTest() throws NoSuchJobException {
+		jobRepository.createJobInstance(JOB_NAME, JobParametersTestUtils.getJobParameters());
+		jobRepository.createJobInstance(JOB_NAME, JobParametersTestUtils.getJobParameters());
+		assertEquals(2, jobExplorer.getJobInstanceCount(JOB_NAME));
+	}
 	
+	@Test(expected=NoSuchJobException.class)
+	public void getJobInstanceCountNoJobException() throws NoSuchJobException {
+		jobExplorer.getJobInstanceCount("NoJobs");
+	}
 
 }
