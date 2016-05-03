cd88ed703c0a6ccee636e43ad02e72e04fb26a43
Scott Stafford <sas4z@yahoo.com>
2016-05-03 17:29:41 -0400

    Replace slf4j dependency with Apache Logging Library

--- src/main/java/com/marklogic/spring/batch/item/AbstractDocumentWriter.java
+++ src/main/java/com/marklogic/spring/batch/item/AbstractDocumentWriter.java
@@ -1,7 +1,7 @@
 package com.marklogic.spring.batch.item;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemStreamSupport;
 
 import com.marklogic.client.io.DocumentMetadataHandle;
@@ -14,7 +14,7 @@ import com.marklogic.uri.UriGenerator;
  */
 public abstract class AbstractDocumentWriter extends ItemStreamSupport {
 
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
+	protected static final Log logger = LogFactory.getLog(AbstractDocumentWriter.class);
 
     private UriGenerator uriGenerator = new DefaultUriGenerator();
 
