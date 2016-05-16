f20f01e2bf04ef270787b0762e6d77f6dd75a35f
Scott Stafford <sas4z@yahoo.com>
2016-05-16 17:04:01 -0400

    Use JAXBMarshaller for Unmarshal tests

--- src/test/java/com/marklogic/spring/batch/bind/UnmarshallSpringBatchPojoTest.java
+++ src/test/java/com/marklogic/spring/batch/bind/UnmarshallSpringBatchPojoTest.java
@@ -4,7 +4,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
 
+import com.marklogic.junit.spring.AbstractSpringTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.core.JobExecution;
@@ -15,6 +22,7 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.marklogic.spring.batch.AbstractSpringBatchTest;
@@ -24,24 +32,29 @@ import com.marklogic.spring.batch.core.AdaptedJobInstance;
 import com.marklogic.spring.batch.core.AdaptedJobParameters;
 import com.marklogic.spring.batch.core.AdaptedStepExecution;
 import com.marklogic.spring.batch.core.MarkLogicJobInstance;
+import org.springframework.test.context.ContextConfiguration;
+import org.w3c.dom.Document;
 
-@ActiveProfiles(profiles = "marklogic", inheritProfiles = false)
-public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
+@ContextConfiguration(classes = {
+		com.marklogic.junit.spring.BasicTestConfig.class,
+		com.marklogic.spring.batch.configuration.MarkLogicBatchConfiguration.class
+})
+public class UnmarshallSpringBatchPojoTest extends AbstractSpringTest {
 	
 	@Autowired
 	private ApplicationContext ctx;
-	
-	private Unmarshaller unmarshaller;
-	
-	@Before
-	public void setup() throws Exception {
-		unmarshaller = jaxbContext().createUnmarshaller();
-	}
+
+	Document doc;
+	DOMSource source;
+
+	@Autowired
+	Jaxb2Marshaller jaxb2Marshaller;
 	
 	@Test
 	public void unmarshallJobParameters() throws Exception {
 		Resource jobParametersXml = ctx.getResource("classpath:/xml/job-parameters.xml");
-		AdaptedJobParameters adParams = (AdaptedJobParameters)unmarshaller.unmarshal(jobParametersXml.getInputStream());
+
+		AdaptedJobParameters adParams = (AdaptedJobParameters)jaxb2Marshaller.unmarshal(new StreamSource(jobParametersXml.getInputStream()));
 		JobParametersAdapter adapter = new JobParametersAdapter();
 		JobParameters params = adapter.unmarshal(adParams);
 		assertEquals(5, params.getParameters().size());
@@ -55,7 +68,7 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	@Test
 	public void unmarshallJobInstance() throws Exception {
 		Resource jobInstanceXml = ctx.getResource("classpath:/xml/job-instance.xml");
-		AdaptedJobInstance adJobInstance = (AdaptedJobInstance)unmarshaller.unmarshal(jobInstanceXml.getInputStream());
+		AdaptedJobInstance adJobInstance = (AdaptedJobInstance)jaxb2Marshaller.unmarshal(new StreamSource(jobInstanceXml.getInputStream()));
 		JobInstanceAdapter adapter = new JobInstanceAdapter();
 		JobInstance jobInstance = adapter.unmarshal(adJobInstance);
 		assertEquals(new Long(123L), jobInstance.getId());
@@ -66,7 +79,7 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	@Test
 	public void unmarshallJobExecution() throws Exception {
 		Resource jobExecutionXml = ctx.getResource("classpath:/xml/job-execution.xml");
-		AdaptedJobExecution adJobExecution = (AdaptedJobExecution)unmarshaller.unmarshal(jobExecutionXml.getInputStream());
+		AdaptedJobExecution adJobExecution = (AdaptedJobExecution)jaxb2Marshaller.unmarshal(new StreamSource(jobExecutionXml.getInputStream()));
 		JobExecutionAdapter adapter = new JobExecutionAdapter();
 		JobExecution jobExecution = adapter.unmarshal(adJobExecution);
 		assertNotNull(jobExecution);
@@ -78,7 +91,7 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	@Test
 	public void unmarshallStepExecution() throws Exception {
 		Resource stepExecutionXml = ctx.getResource("classpath:/xml/step-execution.xml");
-		AdaptedStepExecution adStepExecution = (AdaptedStepExecution)unmarshaller.unmarshal(stepExecutionXml.getInputStream());
+		AdaptedStepExecution adStepExecution = (AdaptedStepExecution)jaxb2Marshaller.unmarshal(new StreamSource(stepExecutionXml.getInputStream()));
 		StepExecutionAdapter adapter = new StepExecutionAdapter();
 		StepExecution stepExecution = adapter.unmarshal(adStepExecution);
 		assertEquals("testStep", stepExecution.getStepName());
@@ -87,7 +100,7 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	@Test
 	public void unmarshallExecutionContext() throws Exception {
 		Resource executionContextXml = ctx.getResource("classpath:/xml/execution-context.xml");
-		AdaptedExecutionContext aec = (AdaptedExecutionContext)unmarshaller.unmarshal(executionContextXml.getInputStream());
+		AdaptedExecutionContext aec = (AdaptedExecutionContext)jaxb2Marshaller.unmarshal(new StreamSource(executionContextXml.getInputStream()));
 		ExecutionContextAdapter adapter = new ExecutionContextAdapter();
 		ExecutionContext executionContext = adapter.unmarshal(aec);
 		assertEquals("testValue", executionContext.get("testName"));
@@ -100,7 +113,7 @@ public class UnmarshallSpringBatchPojoTest extends AbstractSpringBatchTest {
 	@Test
 	public void unmarshallMarkLogicJobInstance() throws Exception {
 		Resource xml = ctx.getResource("classpath:/xml/ml-job-instance.xml");
-		MarkLogicJobInstance mji = (MarkLogicJobInstance)unmarshaller.unmarshal(xml.getInputStream());
+		MarkLogicJobInstance mji = (MarkLogicJobInstance)jaxb2Marshaller.unmarshal(new StreamSource(xml.getInputStream()));
 		assertNotNull(mji);
 		assertEquals(new Long(1930652603895744222L), mji.getJobInstance().getId());
 		assertEquals(1, mji.getJobExecutions().size());
