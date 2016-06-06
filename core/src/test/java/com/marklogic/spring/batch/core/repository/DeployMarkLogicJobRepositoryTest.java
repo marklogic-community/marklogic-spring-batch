a67b5aaf17bf6607c150c677a063902ae4d2bfb6
Scott Stafford <sas4z@yahoo.com>
2016-06-06 17:19:03 -0400

    Add simple assert that REST API exists

--- core/src/test/java/com/marklogic/spring/batch/core/repository/DeployMarkLogicJobRepositoryTest.java
+++ core/src/test/java/com/marklogic/spring/batch/core/repository/DeployMarkLogicJobRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.marklogic.spring.batch.core.repository;
 
+import com.marklogic.mgmt.restapis.RestApiManager;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,17 +9,21 @@ public class DeployMarkLogicJobRepositoryTest {
 
     MarkLogicSimpleJobRepositoryConfig config;
     MarkLogicSimpleJobRepositoryAppDeployer deployer;
+    RestApiManager apiMgr;
 
     @Before
     public void undeployApplication() {
         config = new MarkLogicSimpleJobRepositoryConfig("oscar", 8200, "admin", "admin");
         deployer = new MarkLogicSimpleJobRepositoryAppDeployer(config);
         deployer.undeploy(config.getAppConfig());
+
+        apiMgr = new RestApiManager(config.getManageClient());
+        Assert.assertFalse(apiMgr.restApiServerExists("spring-batch"));
     }
 
     @Test
     public void deployMarkLogicJobRepositoryTest() {
         deployer.deploy(config.getAppConfig());
-
+        Assert.assertTrue(apiMgr.restApiServerExists("spring-batch"));
     }
 }
