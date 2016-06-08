ceed3c8334076f2db6bd5517c635db27b4acaa35
Scott Stafford <sas4z@yahoo.com>
2016-06-07 22:43:24 -0400

    Modify CorbJobTest to work

--- jobs/src/test/java/com/marklogic/spring/batch/job/CorbJobTest.java
+++ jobs/src/test/java/com/marklogic/spring/batch/job/CorbJobTest.java
@@ -1,8 +1,35 @@
 package com.marklogic.spring.batch.job;
 
+import com.marklogic.client.admin.ExtensionLibrariesManager;
+import com.marklogic.client.io.FileHandle;
+import com.marklogic.client.io.Format;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
 
 public class CorbJobTest extends AbstractJobTest {
+
+    File urisModule;
+    File transformModule;
+
+    @Before
+    public void installModules() {
+        ExtensionLibrariesManager libMgr = getClient().newServerConfigManager().newExtensionLibrariesManager();
+        try {
+            urisModule = getApplicationContext().getResource("ml-modules/ext/corb/uris.xqy").getFile();
+            transformModule = getApplicationContext().getResource("ml-modules/ext/corb/process.xqy").getFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileHandle urisHandle = new FileHandle(urisModule).withFormat(Format.TEXT);
+        FileHandle transformHandle = new FileHandle(transformModule).withFormat(Format.TEXT);
+        libMgr.write("/ext/corb/uris.xqy", urisHandle);
+        libMgr.write("/ext/corb/process.xqy", transformHandle);
+    }
 
     @Test
     public void testJob() {
