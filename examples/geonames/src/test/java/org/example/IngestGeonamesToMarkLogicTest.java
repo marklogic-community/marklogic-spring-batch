1e5943a22b02819d13f8c0a111c282fb063ae355
Scott Stafford <sas4z@yahoo.com>
2016-06-17 15:11:09 -0400

    Verify results of geonames ingest

--- examples/geonames/src/test/java/org/example/IngestGeonamesToMarkLogicTest.java
+++ examples/geonames/src/test/java/org/example/IngestGeonamesToMarkLogicTest.java
@@ -11,6 +11,11 @@ public class IngestGeonamesToMarkLogicTest extends AbstractJobTest {
 
     @Test
     public void ingestCitiesTest() {
-        runJobWithMarkLogicJobRepository(IngestGeonamesToMarkLogicConfig.class,  "--uris_module", "/ext/corb/uris.xqy");
+        runJobWithMarkLogicJobRepository(IngestGeonamesToMarkLogicConfig.class, "--chunk", 100);
+        String xquery = "xquery version \"1.0-ml\";\n" +
+                "declare namespace g = \"http://geonames.org\";\n" +
+                "xdmp:estimate(cts:search(fn:doc(), cts:element-query(xs:QName(\"g:geoname\"), cts:and-query(()))))";
+        String result = getClient().newServerEval().xquery(xquery).evalAs(String.class);
+        assertEquals("Expecting 1251 results from US Cities East file", 1251, Integer.parseInt(result));
     }
 }
