d6b4da60f4b8852879d74a2dd65154cdc1f56f81
Rob Rudin <rjrudin@gmail.com>
2016-06-28 22:08:43 -0400

    #115 Made test a lot faster by only loading 10 cities

--- jobs/geonames/src/main/java/com/marklogic/spring/batch/geonames/IngestGeonamesToMarkLogicConfig.java
+++ jobs/geonames/src/main/java/com/marklogic/spring/batch/geonames/IngestGeonamesToMarkLogicConfig.java
@@ -1,7 +1,9 @@
 package com.marklogic.spring.batch.geonames;
 
 import com.marklogic.spring.batch.config.AbstractMarkLogicBatchConfig;
+import com.marklogic.spring.batch.config.support.OptionParserConfigurer;
 import com.marklogic.spring.batch.item.DocumentItemWriter;
+import joptsimple.OptionParser;
 import org.geonames.Geoname;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,26 +12,40 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.w3c.dom.Document;
 
 
 @Configuration
-public class IngestGeonamesToMarkLogicConfig extends AbstractMarkLogicBatchConfig {
+public class IngestGeonamesToMarkLogicConfig extends AbstractMarkLogicBatchConfig implements OptionParserConfigurer{
+
+    @Override
+    public void configureOptionParser(OptionParser parser) {
+        parser.accepts("input_file_path", "The path of the Geonames file to import").withRequiredArg();
+    }
 
     @Bean
     public Job job(@Qualifier("ingestGeonamesStep1") Step step1) {
         return jobBuilderFactory.get("ingestGeonames").start(step1).build();
     }
 
+    /**
+     * TODO Plenty to add here, such as all the options we'd like on a writer - e.g. output collections and
+     * permissions, URI options,e tc.
+     *
+     * @param inputFilePath
+     * @return
+     */
     @Bean
     @JobScope
-    protected Step ingestGeonamesStep1() {
+    protected Step ingestGeonamesStep1(@Value("#{jobParameters['input_file_path']}") String inputFilePath) {
         FlatFileItemReader<Geoname> reader = new FlatFileItemReader<>();
-        reader.setResource(new ClassPathResource("cities_us_east.txt"));
+        reader.setResource(new FileSystemResource(inputFilePath));
         DefaultLineMapper<Geoname> mapper = new DefaultLineMapper<>();
         DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(DelimitedLineTokenizer.DELIMITER_TAB);
         tokenizer.setQuoteCharacter('{');
