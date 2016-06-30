9ceac77b0debc2a8a7e27969831bf0fc950ad685
Scott Stafford <sas4z@yahoo.com>
2016-06-29 23:39:51 -0400

    Add importTriples subproject

--- jobs/importTriples/src/test/java/com/marklogic/spring/batch/config/ImportRdfFromFileTest.java
+++ jobs/importTriples/src/test/java/com/marklogic/spring/batch/config/ImportRdfFromFileTest.java
@@ -1,9 +1,13 @@
 package com.marklogic.spring.batch.config;
 
 import com.marklogic.junit.Fragment;
+import com.marklogic.spring.batch.test.AbstractJobTest;
+import com.marklogic.spring.batch.test.JobProjectTestConfig;
 import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
 
-public class ImportRdfFromFileTest extends AbstractFileImportTest {
+@ContextConfiguration(classes = {JobProjectTestConfig.class})
+public class ImportRdfFromFileTest extends AbstractJobTest {
 
     @Test
     public void test() {
