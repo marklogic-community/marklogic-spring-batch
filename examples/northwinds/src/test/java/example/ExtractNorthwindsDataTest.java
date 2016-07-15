13b0f83f3098c0e5ccda2bc3e176eb9f8c235d25
Scott Stafford <sas4z@yahoo.com>
2016-07-15 17:37:51 -0400

    Need to add this dependency to be picked up in the maven pom

---
+++ examples/northwinds/src/test/java/example/ExtractNorthwindsDataTest.java
@@ -0,0 +1,56 @@
+package example;
+
+import com.marklogic.spring.batch.test.AbstractJobTest;
+import com.marklogic.spring.batch.test.JobProjectTestConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
+
+@ContextConfiguration(classes = {JobProjectTestConfig.class})
+public class ExtractNorthwindsDataTest extends AbstractJobTest {
+    
+    protected static EmbeddedDatabase embeddedDatabase;
+    
+    @Before
+    public void setup() {
+        createDb("db/northwind.sql");
+    }
+    
+    @Test
+    public void verifyNorthwindDatabase() {
+        assertTrue(true);
+    }
+        
+    @After
+    public void teardown() {
+        if (embeddedDatabase != null) {
+            embeddedDatabase.shutdown();
+        }
+        embeddedDatabase = null;
+    }
+    
+    protected void createDb(String... scripts) {
+        embeddedDatabase = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).addScripts(scripts).build();
+    }
+    
+    /**
+     * With our embedded HSQL database, there's not a way that I know of for building a JDBC connection string for it.
+     * So we override this method in the config class that we're testing to inject our own data source.
+     */
+    @Configuration
+    public static class ExtractCommentsFromDatabaseTestConfig extends ExtractNorthwindsDataJob {
+        @Override
+        protected DataSource buildDataSource() {
+            return embeddedDatabase;
+        }
+    }
+    
+
+}
