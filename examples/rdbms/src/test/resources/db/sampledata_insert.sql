7465f891c875bda6ff853409978af0e034a6f901
Scott Stafford <sas4z@yahoo.com>
2016-07-18 21:29:28 -0400

    Add gradle command to run a HSQL database manager

--- examples/rdbms/src/test/resources/db/sampledata_insert.sql
+++ examples/rdbms/src/test/resources/db/sampledata_insert.sql
@@ -1,21 +1,11 @@
 /*
- *  $Id: sampledata.sql 3353 2009-12-15 19:52:13Z unsaved $
+ *  $Id: sampledata.sql 3348 2009-12-15 14:24:19Z unsaved $
  *
  *  Creates and populates database objects with sample data.
  *  This file was created by grabbing the commands made by creating
  *  sample data with the DatabaseManager utility.
- *  http://www.hsqldb.org/doc/verbatim/sample/sampledata.sql
  */
 
-
-DROP TABLE Item IF EXISTS;
-DROP TABLE Invoice IF EXISTS;
-DROP TABLE Product IF EXISTS;
-DROP TABLE Customer IF EXISTS;
-CREATE TABLE Customer(ID INTEGER PRIMARY KEY,FirstName VARCHAR(20),LastName VARCHAR(30),Street VARCHAR(50),City VARCHAR(25));
-CREATE TABLE Product(ID INTEGER PRIMARY KEY,Name VARCHAR(30),Price DECIMAL);
-CREATE TABLE Invoice(ID INTEGER PRIMARY KEY,CustomerID INTEGER,Total DECIMAL, FOREIGN KEY (CustomerId) REFERENCES Customer(ID) ON DELETE CASCADE);
-CREATE TABLE Item(InvoiceID INTEGER,Item INTEGER,ProductID INTEGER,Quantity INTEGER,Cost DECIMAL,PRIMARY KEY(InvoiceID,Item), FOREIGN KEY (InvoiceId) REFERENCES Invoice (ID) ON DELETE CASCADE, FOREIGN KEY (ProductId) REFERENCES Product(ID) ON DELETE CASCADE);
 INSERT INTO Customer VALUES(0,'Laura','Steel','429 Seventh Av.','Dallas');
 INSERT INTO Product VALUES(0,'Iron Iron',54);
 INSERT INTO Customer VALUES(1,'Susanne','King','366 - 20th Ave.','Olten');
