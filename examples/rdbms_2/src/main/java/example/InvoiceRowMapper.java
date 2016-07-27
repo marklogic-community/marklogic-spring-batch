66d62b500cb15b8d3a18a755ba7d8299571df986
Scott Stafford <sas4z@yahoo.com>
2016-07-27 16:17:52 -0400

    Map all row fields to POJO's

--- examples/rdbms_2/src/main/java/example/InvoiceRowMapper.java
+++ examples/rdbms_2/src/main/java/example/InvoiceRowMapper.java
@@ -2,10 +2,13 @@ package example;
 
 import example.data.Customer;
 import example.data.Invoice;
+import example.data.Product;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InvoiceRowMapper implements RowMapper<Invoice> {
     
@@ -14,11 +17,22 @@ public class InvoiceRowMapper implements RowMapper<Invoice> {
         Invoice invoice = new Invoice();
         invoice.setId(rs.getInt("invoice.ID"));
         invoice.setTotal(rs.getFloat("TOTAL"));
+        invoice.setQuantity(rs.getInt("QUANTITY"));
         
         Customer customer = new Customer();
         customer.setId(rs.getInt("CUSTOMERID"));
-        
+        customer.setFirstName(rs.getString("FIRSTNAME"));
+        customer.setLastName(rs.getString("LASTNAME"));
         invoice.setCustomer(customer);
+        
+        Product product = new Product();
+        product.setId(rs.getInt("product.ID"));
+        product.setName(rs.getString("NAME"));
+        product.setPrice(rs.getFloat("PRICE"));
+        
+        List<Product> products = new ArrayList<Product>();
+        products.add(product);
+        invoice.setProducts(products);
         
         return invoice;
     }
