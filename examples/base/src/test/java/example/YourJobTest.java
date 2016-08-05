3a651673193ff2dea9395dda959828c43bfdd7bb
Scott Stafford <sas4z@yahoo.com>
2016-08-05 14:42:33 -0400

    Add simple starter job

--- examples/base/src/test/java/example/YourJobTest.java
+++ examples/base/src/test/java/example/YourJobTest.java
@@ -1,6 +1,28 @@
 package example;
 
+import com.marklogic.client.spring.BasicConfig;
+import com.marklogic.junit.ClientTestHelper;
 import com.marklogic.spring.batch.test.AbstractJobTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = {BasicConfig.class})
 public class YourJobTest extends AbstractJobTest {
+
+    private ClientTestHelper client;
+    
+    @Before
+    public void setup() {
+        client = new ClientTestHelper();
+        client.setDatabaseClientProvider(getClientProvider());
+    }
+
+    @Test
+    public void findZeroMonstersInDatabaseTest() {
+        runJob(
+                YourJobConfig.class,
+                "--output_collections", "monster");
+        client.assertCollectionSize("Expecting 1 items in monster collection", "monster", 1);
+    }
 }
