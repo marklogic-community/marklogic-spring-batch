c7d45f00c98ca8e52e6728f6e9159addc9215d02
Scott Stafford <sstafford@marklogic.com>
2016-09-30 15:39:11 -0400

    Remove dependency to AbstractMarkLogicBatchConfig

--- examples/entityEnrichment/src/main/java/example/EntityEnrichmentJobConfig.java
+++ examples/entityEnrichment/src/main/java/example/EntityEnrichmentJobConfig.java
@@ -1,10 +1,15 @@
 package example;
 
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.helper.DatabaseClientProvider;
 import com.marklogic.client.query.CountedDistinctValue;
 import com.marklogic.spring.batch.config.AbstractMarkLogicBatchConfig;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -13,30 +18,35 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
-public class EntityEnrichmentJobConfig extends AbstractMarkLogicBatchConfig implements EnvironmentAware {
+@EnableBatchProcessing
+public class EntityEnrichmentJobConfig implements EnvironmentAware {
     
     private Environment env;
     
     private final String JOB_NAME = "entityEnrichmentJob";
     
     @Bean
-    public Job job(Step step) {
+    public Job job(JobBuilderFactory jobBuilderFactory, Step step) {
         return jobBuilderFactory.get(JOB_NAME).start(step).build();
     }
     
     @Bean
     @JobScope
-    public Step step(@Value("#{jobParameters['tokenizer_model']}") String tokenizerModel,
-                     @Value("#{jobParameters['named_entity_model']}") String namedEntityModel) {
+    public Step step(
+        StepBuilderFactory stepBuilderFactory,
+        DatabaseClientProvider databaseClientProvider,
+        @Value("#{jobParameters['tokenizer_model']}") String tokenizerModel,
+        @Value("#{jobParameters['named_entity_model']}") String namedEntityModel) {
         
-        ItemReader<CountedDistinctValue> reader = new ValuesItemReader(getDatabaseClient());
+        DatabaseClient databaseClient = databaseClientProvider.getDatabaseClient();
+        ItemReader<CountedDistinctValue> reader = new ValuesItemReader(databaseClient);
         ItemProcessor<CountedDistinctValue, String[]> processor =
-            new EntityEnrichmentItemProcessor(getDatabaseClient(), tokenizerModel, namedEntityModel);
-        ItemWriter<String[]> writer = new MarkLogicPatchItemWriter(getDatabaseClient());
+            new EntityEnrichmentItemProcessor(databaseClient, tokenizerModel, namedEntityModel);
+        ItemWriter<String[]> writer = new MarkLogicPatchItemWriter(databaseClient);
            
         
         return stepBuilderFactory.get("step1")
-                .<CountedDistinctValue, String[]>chunk(getChunkSize())
+                .<CountedDistinctValue, String[]>chunk(10)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
