50afe0f63517a4f02fbd3242843cef605ececbd6
Scott Stafford <sstafford@marklogic.com>
2016-10-01 17:18:44 -0400

    Modify build.gradle to work with root build.gradle in examples directory

--- examples/importDocuments/importDocuments.bat
+++ examples/importDocuments/importDocuments.bat
@@ -1,5 +1,5 @@
 build\install\importDocuments\bin\importDocuments.bat ^
-  --host oscar --port 8200 --username admin --password admin ^
+  --host localhost --port 8200 --username admin --password admin ^
   --config com.marklogic.spring.batch.config.ImportDocumentsFromDirectoryConfig ^
   --input_file_path src/test/resources/data/
   --jrHost oscar --jrPort 8200 --jrUsername admin --jrPassword admin
