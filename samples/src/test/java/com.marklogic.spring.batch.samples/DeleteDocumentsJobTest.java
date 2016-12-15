8ad52a7b1f4103f46271cd9d542ddf85f28aabc2
Scott Stafford <sstafford@marklogic.com>
2016-12-15 11:56:39 -0500

    Push common functions to AbstractJobTest

--- samples/src/test/java/com.marklogic.spring.batch.samples/DeleteDocumentsJobTest.java
+++ samples/src/test/java/com.marklogic.spring.batch.samples/DeleteDocumentsJobTest.java
@@ -2,37 +2,21 @@ package com.marklogic.spring.batch.samples;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.document.XMLDocumentManager;
-import com.marklogic.client.helper.DatabaseClientProvider;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.StringHandle;
-import com.marklogic.junit.ClientTestHelper;
-import com.marklogic.junit.spring.AbstractSpringTest;
+import com.marklogic.spring.batch.test.AbstractJobTest;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = {
-        com.marklogic.spring.batch.samples.DeleteDocumentsJob.class,
-        com.marklogic.spring.batch.test.JobRunnerContext.class,
-        com.marklogic.spring.batch.test.JobProjectTestConfig.class})
-public class DeleteDocumentsJobTest extends AbstractSpringTest {
-
-    @Autowired
-    private JobLauncherTestUtils jobLauncherTestUtils;
-
-    @Autowired
-    DatabaseClientProvider databaseClientProvider;
-
-    private ClientTestHelper clientTestHelper;
+@ContextConfiguration(classes = {com.marklogic.spring.batch.samples.DeleteDocumentsJob.class} )
+public class DeleteDocumentsJobTest extends AbstractJobTest {
 
     public void insertDocument(String uri, String collections, String xml) {
-        clientTestHelper = new ClientTestHelper();
-        clientTestHelper.setDatabaseClientProvider(databaseClientProvider);
         DatabaseClient client = databaseClientProvider.getDatabaseClient();
         XMLDocumentManager docMgr = client.newXMLDocumentManager();
         DocumentMetadataHandle metadata = new DocumentMetadataHandle();
@@ -43,6 +27,7 @@ public class DeleteDocumentsJobTest extends AbstractSpringTest {
 
     @Before
     public void setup() {
+        Assume.assumeTrue(isMarkLogic9());
         for (int i = 0; i < 10; i++) {
             insertDocument("doc" + i, "monster", "<hello />");
         }
@@ -54,7 +39,7 @@ public class DeleteDocumentsJobTest extends AbstractSpringTest {
         jpb.addString("output_collections", "monster");
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jpb.toJobParameters());
         assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
-        clientTestHelper.assertCollectionSize("Expecting zero documents in monster collection", "monster", 0);
+        getClientTestHelper().assertCollectionSize("Expecting zero documents in monster collection", "monster", 0);
     }
 
 }
--- samples/src/test/java/com.marklogic.spring.batch.samples/YourJobTest.java
+++ samples/src/test/java/com.marklogic.spring.batch.samples/YourJobTest.java
@@ -1,49 +1,22 @@
 package com.marklogic.spring.batch.samples;
 
-import com.marklogic.client.helper.DatabaseClientProvider;
-import com.marklogic.junit.ClientTestHelper;
-import com.marklogic.junit.spring.AbstractSpringTest;
+import com.marklogic.spring.batch.test.AbstractJobTest;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = {
-        com.marklogic.spring.batch.samples.YourJob.class,
-        com.marklogic.spring.batch.test.JobRunnerContext.class,
-        com.marklogic.spring.batch.test.JobProjectTestConfig.class})
-public class YourJobTest extends AbstractSpringTest {
-
-    @Autowired
-    private JobLauncherTestUtils jobLauncherTestUtils;
-
-    @Autowired
-    DatabaseClientProvider databaseClientProvider;
-
-    private ClientTestHelper client;
-    
-    @Before
-    public void setup() {
-        client = new ClientTestHelper();
-        client.setDatabaseClientProvider(databaseClientProvider);
-    }
-
-    @Test
-    public void confirmJobCompletesTest() throws Exception {
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
-    }
+@ContextConfiguration(classes = {com.marklogic.spring.batch.samples.YourJob.class})
+public class YourJobTest extends AbstractJobTest {
 
     @Test
     public void findOneMonsterInDatabaseTest() throws Exception {
         JobParametersBuilder jpb = new JobParametersBuilder();
         jpb.addString("output_collections", "monster");
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jpb.toJobParameters());
-        client.assertCollectionSize("Expecting 1 items in monster collection", "monster", 1);
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        getClientTestHelper().assertCollectionSize("Expecting 1 items in monster collection", "monster", 1);
     }
 
 }
