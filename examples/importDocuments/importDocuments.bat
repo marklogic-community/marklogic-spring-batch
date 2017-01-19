38bb2ace1be158481b3c8812452738c6ebdf5684
Scott Stafford <sstafford@marklogic.com>
2017-01-19 16:00:45 -0500

    Remove importDocuments from examples

--- examples/importDocuments/importDocuments.bat
+++
@@ -1,5 +0,0 @@
-build\install\importDocuments\bin\importDocuments.bat ^
-  --host localhost --port 8200 --username admin --password admin ^
-  --config com.marklogic.spring.batch.config.ImportDocumentsFromDirectoryConfig ^
-  --input_file_path src/test/resources/data/
-  --jrHost oscar --jrPort 8200 --jrUsername admin --jrPassword admin
