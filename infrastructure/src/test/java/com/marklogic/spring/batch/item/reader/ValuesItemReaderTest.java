ae4bd210562df942d98f20c32acce06f0415578d
Scott Stafford <sstafford@marklogic.com>
2017-03-03 15:57:02 -0500

    Make ValuesItemReader more extensible

--- infrastructure/src/test/java/com/marklogic/spring/batch/item/reader/ValuesItemReaderTest.java
+++ infrastructure/src/test/java/com/marklogic/spring/batch/item/reader/ValuesItemReaderTest.java
@@ -11,6 +11,7 @@ import com.marklogic.junit.ClientTestHelper;
 import com.marklogic.junit.spring.AbstractSpringTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -30,33 +31,44 @@ public class ValuesItemReaderTest extends AbstractSpringTest {
         client = DatabaseClientFactory.newClient(databaseClientConfig.getHost(), databaseClientConfig.getPort(), securityContext);
         helper = new ClientTestHelper();
         helper.setDatabaseClientProvider(getClientProvider());
+
         XMLDocumentManager docMgr = client.newXMLDocumentManager();
+
         StringHandle xml1 = new StringHandle("<hello />");
         DocumentMetadataHandle metadata = new DocumentMetadataHandle();
         metadata.withCollections("a");
     
         DocumentMetadataHandle metadata2 = new DocumentMetadataHandle();
-        metadata2.withCollections("sourceXML");
+        metadata2.withCollections("b");
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 600; i++) {
             DocumentMetadataHandle h = (i % 2 == 0) ? metadata : metadata2;
             docMgr.write("hello" + i + ".xml", h, xml1);
         }
-        helper.assertCollectionSize("", "a", 5);
-        helper.assertCollectionSize("", "sourceXML", 5);
+        helper.assertCollectionSize("a = 300", "a", 300);
+        helper.assertCollectionSize("b = 300", "b", 300);
     }
     
     @Test
-    public void getValuesFromItemReaderTest() throws Exception {
-        reader = new ValuesItemReader(client);
-        reader.open(null);
-        assertEquals("Expecting size of 5", reader.getLength(), 5);
+    public void getUriValuesFromItemReaderTest() throws Exception {
+        String uriQueryOptions =
+                "<options xmlns=\"http://marklogic.com/appservices/search\">\n" +
+                        "    <search-option>unfiltered</search-option>\n" +
+                        "    <quality-weight>0</quality-weight>\n" +
+                        "    <values name=\"uris\">\n" +
+                        "        <uri/>\n" +
+                        "    </values>\n" +
+                        "</options>";
+        reader = new ValuesItemReader(client, new StringHandle(uriQueryOptions), "uris");
+        reader.open(new ExecutionContext());
+        assertEquals("Expecting size of 600", reader.getLength(), 600);
         CountedDistinctValue val = reader.read();
         String uri = val.get("xs:string", String.class);
         logger.info(uri);
-        assertTrue(uri.equals("hello1.xml"));
+        assertTrue(uri.equals("hello0.xml"));
         val = reader.read();
         uri = val.get("xs:string", String.class);
-        assertTrue(uri.equals("hello3.xml"));
+        assertTrue(uri.equals("hello1.xml"));
+        reader.close();
     }
 }
