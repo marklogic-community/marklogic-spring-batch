f8d8f01064f798c202212423ef2c9c002ee60c4b
Scott Stafford <sstafford@marklogic.com>
2017-03-06 17:06:17 -0500

    Add InvokeModuleItemWriter to support corb jobs

---
+++ infrastructure/src/main/java/com/marklogic/spring/batch/item/writer/InvokeModuleItemWriter.java
@@ -0,0 +1,70 @@
+package com.marklogic.spring.batch.item.writer;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.eval.EvalResult;
+import com.marklogic.client.eval.EvalResultIterator;
+import com.marklogic.client.eval.ServerEvaluationCall;
+import com.marklogic.client.helper.DatabaseClientProvider;
+import com.marklogic.client.io.StringHandle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+
+import org.jdom2.Namespace;
+import java.util.List;
+import java.util.Map;
+
+public class InvokeModuleItemWriter implements ItemWriter<Map<String, String>> {
+
+    protected String module;
+    protected DatabaseClient databaseClient;
+    protected ServerEvaluationCall invoker;
+    protected List<Namespace> namespaces;
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public InvokeModuleItemWriter(DatabaseClient databaseClient, String module) {
+        this.module = module;
+        this.databaseClient = databaseClient;
+        invoker = databaseClient.newServerEval();
+        invoker.modulePath(module);
+    }
+
+    public InvokeModuleItemWriter(DatabaseClient databaseClient, String module, List<Namespace> namespaces) {
+        this(databaseClient, module);
+        for (Namespace namespace : namespaces) {
+            invoker.addNamespace(namespace.getPrefix(), namespace.getURI());
+        }
+    }
+
+    @Override
+    public void write(List<? extends Map<String, String>> items) throws Exception {
+        for (Map<String, String> item : items) {
+            for (Map.Entry<String, String> name : item.entrySet()) {
+                invoker.addVariable(name.getKey(), name.getValue());
+            }
+            processResults(invoker.eval());
+
+        }
+    }
+
+    public void processResults(EvalResultIterator itr) {
+        String item = null;
+        while (itr.hasNext()) {
+            EvalResult result = itr.next();
+            switch (result.getType()) {
+                case STRING: {
+                    item = result.getAs(String.class);
+                    logger.info(item);
+                    break;
+                }
+                default: {
+                    StringHandle handle = result.get(new StringHandle());
+                    logger.info(handle.get());
+                    break;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ infrastructure/src/test/java/com/marklogic/spring/batch/item/writer/InvokeModuleItemWriterTest.java
@@ -0,0 +1,64 @@
+package com.marklogic.spring.batch.item.writer;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.admin.ExtensionLibrariesManager;
+import com.marklogic.client.helper.DatabaseClientConfig;
+import com.marklogic.client.helper.DatabaseClientProvider;
+import com.marklogic.client.io.Format;
+import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.spring.SimpleDatabaseClientProvider;
+import com.marklogic.junit.ClientTestHelper;
+import com.marklogic.junit.spring.AbstractSpringTest;
+import com.marklogic.spring.batch.test.SpringBatchNamespaceProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ContextConfiguration(classes = { com.marklogic.spring.batch.config.MarkLogicApplicationContext.class })
+public class InvokeModuleItemWriterTest extends AbstractSpringTest {
+
+    InvokeModuleItemWriter itemWriter;
+    ExtensionLibrariesManager libMgr;
+    final String MODULE_PATH = "/ext/test.xqy";
+    ClientTestHelper clientTestHelper;
+
+    @Autowired
+    public void setClientTestHelper(DatabaseClientConfig databaseClientConfig) {
+        clientTestHelper = new ClientTestHelper();
+        DatabaseClientProvider databaseClientProvider = new SimpleDatabaseClientProvider(databaseClientConfig);
+        clientTestHelper.setDatabaseClientProvider(databaseClientProvider);
+        clientTestHelper.setNamespaceProvider(new SpringBatchNamespaceProvider());
+        return;
+    }
+
+    @Before
+    public void setup() {
+        DatabaseClient client = getClient();
+        libMgr = client.newServerConfigManager().newExtensionLibrariesManager();
+        StringHandle handle = new StringHandle("xdmp:document-insert('hello.xml', <hello />, (), 'test')").withFormat(Format.TEXT);
+        libMgr.writeAs(MODULE_PATH, handle);
+        itemWriter = new InvokeModuleItemWriter(client, MODULE_PATH);
+    }
+
+    @Test
+    public void invokeModuleItemWriterTest() throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        List<Map<String, String>> maps = new ArrayList<Map<String, String>>();
+        maps.add(map);
+        itemWriter.write(maps);
+        clientTestHelper.assertInCollections("hello.xml", "test");
+
+    }
+
+    @After
+    public void teardown() {
+        libMgr.delete(MODULE_PATH);
+    }
+}
