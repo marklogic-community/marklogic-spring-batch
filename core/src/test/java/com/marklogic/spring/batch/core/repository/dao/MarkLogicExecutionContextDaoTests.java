e5191cb8de7e02c7b483edc10d177f55b08afae5
Scott Stafford <sstafford@marklogic.com>
2017-10-31 01:18:05 -0400

    Pretty format execContextDao

--- core/src/test/java/com/marklogic/spring/batch/core/repository/dao/MarkLogicExecutionContextDaoTests.java
+++ core/src/test/java/com/marklogic/spring/batch/core/repository/dao/MarkLogicExecutionContextDaoTests.java
@@ -18,158 +18,160 @@ import java.util.List;
 
 public class MarkLogicExecutionContextDaoTests extends AbstractJobRepositoryTest {
 
-	private ExecutionContextDao dao;
-	private JobInstanceDao jobInstanceDao;
-	private JobExecutionDao jobExecutionDao;
-	private StepExecutionDao stepExecutionDao;
-	private JobExecution jobExecution;
-	private StepExecution stepExecution;
+    private ExecutionContextDao dao;
+    private JobInstanceDao jobInstanceDao;
+    private JobExecutionDao jobExecutionDao;
+    private StepExecutionDao stepExecutionDao;
+    private JobExecution jobExecution;
+    private StepExecution stepExecution;
 
-	@Before
-	public void setUp() {
-		dao = new MarkLogicExecutionContextDao(null, null);
-		JobInstance ji = jobInstanceDao.createJobInstance("testJob", new JobParameters());
-		jobExecution = new JobExecution(ji, new JobParameters());
-		jobExecutionDao.saveJobExecution(jobExecution);
-		stepExecution = new StepExecution("stepName", jobExecution);
-		stepExecutionDao.saveStepExecution(stepExecution);
+    @Before
+    public void setUp() {
+        JobExecutionDao jobExecutionDao = new MarkLogicJobExecutionDao(getClient(), getBatchProperties());
+        StepExecutionDao stepExecutionDao = new MarkLogicStepExecutionDao(getClient(), jobExecutionDao, getBatchProperties());
+        dao = new MarkLogicExecutionContextDao(jobExecutionDao, stepExecutionDao);
+        JobInstance ji = jobInstanceDao.createJobInstance("testJob", new JobParameters());
+        jobExecution = new JobExecution(ji, new JobParameters());
+        jobExecutionDao.saveJobExecution(jobExecution);
+        stepExecution = new StepExecution("stepName", jobExecution);
+        stepExecutionDao.saveStepExecution(stepExecution);
 
-	}
+    }
 
-	@Transactional
-	@Test
-	public void testSaveAndFindJobContext() {
-		ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
-		jobExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(jobExecution);
+    @Transactional
+    @Test
+    public void testSaveAndFindJobContext() {
+        ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
+        jobExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(jobExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
-		assertEquals(ctx, retrieved);
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
+        assertEquals(ctx, retrieved);
+    }
 
-	@Transactional
-	@Test
-	public void testSaveAndFindExecutionContexts() {
+    @Transactional
+    @Test
+    public void testSaveAndFindExecutionContexts() {
 
-		List<StepExecution> stepExecutions = new ArrayList<>();
-		for (int i = 0; i < 3; i++) {
-			JobInstance ji = jobInstanceDao.createJobInstance("testJob" + i, new JobParameters());
-			JobExecution je = new JobExecution(ji, new JobParameters());
-			jobExecutionDao.saveJobExecution(je);
-			StepExecution se = new StepExecution("step" + i, je);
-			se.setStatus(BatchStatus.STARTED);
-			se.setReadSkipCount(i);
-			se.setProcessSkipCount(i);
-			se.setWriteSkipCount(i);
-			se.setProcessSkipCount(i);
-			se.setRollbackCount(i);
-			se.setLastUpdated(new Date(System.currentTimeMillis()));
-			se.setReadCount(i);
-			se.setFilterCount(i);
-			se.setWriteCount(i);
-			stepExecutions.add(se);
-		}
-		stepExecutionDao.saveStepExecutions(stepExecutions);
-		dao.saveExecutionContexts(stepExecutions);
+        List<StepExecution> stepExecutions = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            JobInstance ji = jobInstanceDao.createJobInstance("testJob" + i, new JobParameters());
+            JobExecution je = new JobExecution(ji, new JobParameters());
+            jobExecutionDao.saveJobExecution(je);
+            StepExecution se = new StepExecution("step" + i, je);
+            se.setStatus(BatchStatus.STARTED);
+            se.setReadSkipCount(i);
+            se.setProcessSkipCount(i);
+            se.setWriteSkipCount(i);
+            se.setProcessSkipCount(i);
+            se.setRollbackCount(i);
+            se.setLastUpdated(new Date(System.currentTimeMillis()));
+            se.setReadCount(i);
+            se.setFilterCount(i);
+            se.setWriteCount(i);
+            stepExecutions.add(se);
+        }
+        stepExecutionDao.saveStepExecutions(stepExecutions);
+        dao.saveExecutionContexts(stepExecutions);
 
-		for (int i = 0; i < 3; i++) {
-			ExecutionContext retrieved = dao.getExecutionContext(stepExecutions.get(i).getJobExecution());
-			assertEquals(stepExecutions.get(i).getExecutionContext(), retrieved);
-		}
-	}
+        for (int i = 0; i < 3; i++) {
+            ExecutionContext retrieved = dao.getExecutionContext(stepExecutions.get(i).getJobExecution());
+            assertEquals(stepExecutions.get(i).getExecutionContext(), retrieved);
+        }
+    }
 
-	@Transactional
-	@Test(expected = IllegalArgumentException.class)
-	public void testSaveNullExecutionContexts() {
-		dao.saveExecutionContexts(null);
-	}
+    @Transactional
+    @Test(expected = IllegalArgumentException.class)
+    public void testSaveNullExecutionContexts() {
+        dao.saveExecutionContexts(null);
+    }
 
-	@Transactional
-	@Test
-	public void testSaveEmptyExecutionContexts() {
-		dao.saveExecutionContexts(new ArrayList<StepExecution>());
-	}
+    @Transactional
+    @Test
+    public void testSaveEmptyExecutionContexts() {
+        dao.saveExecutionContexts(new ArrayList<StepExecution>());
+    }
 
-	@Transactional
-	@Test
-	public void testSaveAndFindEmptyJobContext() {
+    @Transactional
+    @Test
+    public void testSaveAndFindEmptyJobContext() {
 
-		ExecutionContext ctx = new ExecutionContext();
-		jobExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(jobExecution);
+        ExecutionContext ctx = new ExecutionContext();
+        jobExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(jobExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
-		assertEquals(ctx, retrieved);
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
+        assertEquals(ctx, retrieved);
+    }
 
-	@Transactional
-	@Test
-	public void testUpdateContext() {
+    @Transactional
+    @Test
+    public void testUpdateContext() {
 
-		ExecutionContext ctx = new ExecutionContext(Collections
-				.singletonMap("key", "value"));
-		jobExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(jobExecution);
+        ExecutionContext ctx = new ExecutionContext(Collections
+                .singletonMap("key", "value"));
+        jobExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(jobExecution);
 
-		ctx.putLong("longKey", 7);
-		dao.updateExecutionContext(jobExecution);
+        ctx.putLong("longKey", 7);
+        dao.updateExecutionContext(jobExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
-		assertEquals(ctx, retrieved);
-		assertEquals(7, retrieved.getLong("longKey"));
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(jobExecution);
+        assertEquals(ctx, retrieved);
+        assertEquals(7, retrieved.getLong("longKey"));
+    }
 
-	@Transactional
-	@Test
-	public void testSaveAndFindStepContext() {
+    @Transactional
+    @Test
+    public void testSaveAndFindStepContext() {
 
-		ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
-		stepExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(stepExecution);
+        ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
+        stepExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(stepExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
-		assertEquals(ctx, retrieved);
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
+        assertEquals(ctx, retrieved);
+    }
 
-	@Transactional
-	@Test
-	public void testSaveAndFindEmptyStepContext() {
+    @Transactional
+    @Test
+    public void testSaveAndFindEmptyStepContext() {
 
-		ExecutionContext ctx = new ExecutionContext();
-		stepExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(stepExecution);
+        ExecutionContext ctx = new ExecutionContext();
+        stepExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(stepExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
-		assertEquals(ctx, retrieved);
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
+        assertEquals(ctx, retrieved);
+    }
 
-	@Transactional
-	@Test
-	public void testUpdateStepContext() {
+    @Transactional
+    @Test
+    public void testUpdateStepContext() {
 
-		ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
-		stepExecution.setExecutionContext(ctx);
-		dao.saveExecutionContext(stepExecution);
+        ExecutionContext ctx = new ExecutionContext(Collections.singletonMap("key", "value"));
+        stepExecution.setExecutionContext(ctx);
+        dao.saveExecutionContext(stepExecution);
 
-		ctx.putLong("longKey", 7);
-		dao.updateExecutionContext(stepExecution);
+        ctx.putLong("longKey", 7);
+        dao.updateExecutionContext(stepExecution);
 
-		ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
-		assertEquals(ctx, retrieved);
-		assertEquals(7, retrieved.getLong("longKey"));
-	}
+        ExecutionContext retrieved = dao.getExecutionContext(stepExecution);
+        assertEquals(ctx, retrieved);
+        assertEquals(7, retrieved.getLong("longKey"));
+    }
 
-	@Transactional
-	@Test
-	public void testStoreInteger() {
+    @Transactional
+    @Test
+    public void testStoreInteger() {
 
-		ExecutionContext ec = new ExecutionContext();
-		ec.put("intValue", 343232);
-		stepExecution.setExecutionContext(ec);
-		dao.saveExecutionContext(stepExecution);
-		ExecutionContext restoredEc = dao.getExecutionContext(stepExecution);
-		assertEquals(ec, restoredEc);
-	}
+        ExecutionContext ec = new ExecutionContext();
+        ec.put("intValue", 343232);
+        stepExecution.setExecutionContext(ec);
+        dao.saveExecutionContext(stepExecution);
+        ExecutionContext restoredEc = dao.getExecutionContext(stepExecution);
+        assertEquals(ec, restoredEc);
+    }
 
 
 }
