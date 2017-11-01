08198b6fc23a15b1f91188c6e840d3eef2105915
Scott Stafford <sstafford@marklogic.com>
2017-10-31 23:06:11 -0400

    Fix jobInstances test

--- core/src/test/java/com/marklogic/spring/batch/core/explore/GetJobInstancesTest.java
+++ core/src/test/java/com/marklogic/spring/batch/core/explore/GetJobInstancesTest.java
@@ -1,13 +1,19 @@
 package com.marklogic.spring.batch.core.explore;
 
 import com.marklogic.spring.batch.JobParametersTestUtils;
+import com.marklogic.spring.batch.core.repository.dao.MarkLogicExecutionContextDao;
+import com.marklogic.spring.batch.core.repository.dao.MarkLogicJobExecutionDao;
+import com.marklogic.spring.batch.core.repository.dao.MarkLogicJobInstanceDao;
+import com.marklogic.spring.batch.core.repository.dao.MarkLogicStepExecutionDao;
 import com.marklogic.spring.batch.test.AbstractJobRepositoryTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.SimpleJobExplorer;
 import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.SimpleJobRepository;
 
 import java.util.List;
 
@@ -16,13 +22,24 @@ public class GetJobInstancesTest extends AbstractJobRepositoryTest {
     private final String JOB_NAME = "testJob";
     private final String JOB_NAME_2 = JOB_NAME + "2";
     private final String JOB_NAME_3 = JOB_NAME + "3";
-    
+
     private JobRepository jobRepository;
     private JobExplorer jobExplorer;
 
     @Before
     public void initialize() {
-        
+        jobRepository = new SimpleJobRepository(
+                new MarkLogicJobInstanceDao(getClient(), getBatchProperties()),
+                new MarkLogicJobExecutionDao(getClient(), getBatchProperties()),
+                new MarkLogicStepExecutionDao(getClient(), getBatchProperties()),
+                new MarkLogicExecutionContextDao(getClient(), getBatchProperties())
+        );
+        jobExplorer = new SimpleJobExplorer(
+                new MarkLogicJobInstanceDao(getClient(), getBatchProperties()),
+                new MarkLogicJobExecutionDao(getClient(), getBatchProperties()),
+                new MarkLogicStepExecutionDao(getClient(), getBatchProperties()),
+                new MarkLogicExecutionContextDao(getClient(), getBatchProperties())
+        );
     }
 
     @Test
