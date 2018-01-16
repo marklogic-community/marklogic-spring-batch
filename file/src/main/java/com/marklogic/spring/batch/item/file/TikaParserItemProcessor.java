f29d4d23d2103f4bb912b8a8591002038018fc06
Scott Stafford <sstafford@marklogic.com>
2018-01-16 12:39:06 -0500

    #279 add TikaParserItemProcessor

---
+++ file/src/main/java/com/marklogic/spring/batch/item/file/TikaParserItemProcessor.java
@@ -0,0 +1,23 @@
+package com.marklogic.spring.batch.item.file;
+
+import com.marklogic.client.io.InputSourceHandle;
+import com.marklogic.client.io.marker.AbstractWriteHandle;
+import com.marklogic.spring.batch.item.file.support.TikaParser;
+import com.marklogic.spring.batch.item.processor.AbstractMarkLogicItemProcessor;
+import org.springframework.core.io.Resource;
+import org.xml.sax.InputSource;
+
+import java.io.StringReader;
+
+public class TikaParserItemProcessor extends AbstractMarkLogicItemProcessor<Resource> {
+
+    @Override
+    public AbstractWriteHandle getContentHandle(Resource item) throws Exception {
+        String parsedContent = TikaParser.parseToXML(item.getInputStream());
+        InputSource inputSource = new InputSource(new StringReader(parsedContent.toString()));
+        InputSourceHandle handle = new InputSourceHandle(inputSource);
+        return handle;
+    }
+
+
+}
