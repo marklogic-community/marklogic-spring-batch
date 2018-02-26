f0126fbe45339dffbd18f32e159e157ec918eb95
Chunyu <chunyu.wilson@marklogic.com>
2018-02-26 20:41:10 +0000

    Insert a fragment into an existing document #295

--- core/src/main/java/com/marklogic/spring/batch/item/writer/MarkLogicPatchItemWriter.java
+++ core/src/main/java/com/marklogic/spring/batch/item/writer/MarkLogicPatchItemWriter.java
@@ -1,14 +1,23 @@
 package com.marklogic.spring.batch.item.writer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.document.DocumentPatchBuilder;
+import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.io.marker.DocumentPatchHandle;
 import com.marklogic.client.util.EditableNamespaceContext;
+import jdk.internal.org.xml.sax.SAXException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class MarkLogicPatchItemWriter implements ItemWriter<String[]> {
@@ -19,24 +28,72 @@ public class MarkLogicPatchItemWriter implements ItemWriter<String[]> {
     public MarkLogicPatchItemWriter(DatabaseClient client) {
         this.databaseClient = client;
     }
-    
+
     @Override
     public void write(List<? extends String[]> items) throws Exception {
         for (String[] item : items) {
             String uri = item[0];
-            String xmlPatch = item[1];
+            String itemPatch = item[1];
             logger.info(uri);
-            XMLDocumentManager docMgr = databaseClient.newXMLDocumentManager();
+
+            XMLDocumentManager docXmlMgr = databaseClient.newXMLDocumentManager();
+            JSONDocumentManager docJsonMgr = databaseClient.newJSONDocumentManager();
+
             EditableNamespaceContext namespaces = new EditableNamespaceContext();
             namespaces.put("html", "http://www.w3.org/1999/xhtml");
-            
-            DocumentPatchBuilder xmlPatchBldr = docMgr.newPatchBuilder();
+
+            DocumentPatchBuilder xmlPatchBldr = docXmlMgr.newPatchBuilder();
             xmlPatchBldr.setNamespaces(namespaces);
-        
-            //note the root element is referenced in the first parameter of this call, you may need to change based on your document
-            DocumentPatchHandle patchHandle = xmlPatchBldr.insertFragment("/doc", DocumentPatchBuilder.Position.LAST_CHILD, xmlPatch).build();
-            docMgr.patch(uri, patchHandle);
+
+            DocumentPatchBuilder jsonPatchBldr = docJsonMgr.newPatchBuilder();
+
+            Boolean isXml = false;
+
+            if (itemPatch.startsWith("<")) {isXml = checkIfXMLIsWellFormed(itemPatch); }
+
+            Boolean isJson = isJSONValid(itemPatch);
+
+            if (isXml) {
+                //note the root element is referenced in the first parameter of this call, you may need to change based on your xml document
+                DocumentPatchHandle patchHandle = xmlPatchBldr.insertFragment("/doc", DocumentPatchBuilder.Position.LAST_CHILD, itemPatch).build();
+                docXmlMgr.patch(uri, patchHandle);
+            }
+
+            if (isJson)
+            {
+                //note the root node is referenced in the first parameter of this call, you may need to change based on your json document
+                DocumentPatchHandle patchHandle = jsonPatchBldr.insertFragment("a", DocumentPatchBuilder.Position.LAST_CHILD, itemPatch).build();
+                docJsonMgr.patch(uri, patchHandle);
+            }
+
+
         }
-        
+
     }
+
+
+    private boolean checkIfXMLIsWellFormed(String xml) throws SAXException, IOException, org.xml.sax.SAXException {
+        XMLReader parser = XMLReaderFactory.createXMLReader();
+        parser.setContentHandler(new DefaultHandler());
+        InputSource source = new InputSource(new ByteArrayInputStream(xml.getBytes()));
+        try {
+            parser.parse(source);
+            return true;
+        } catch (org.xml.sax.SAXException e) {
+            System.out.println(xml + "not well formed");
+            return false;
+        }
+    }
+
+
+    public static boolean isJSONValid(String jsonInString ) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.readTree(jsonInString);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
 }
--- core/src/test/java/com/marklogic/spring/batch/item/writer/MarkLogicPatchItemWriterTest.java
+++ core/src/test/java/com/marklogic/spring/batch/item/writer/MarkLogicPatchItemWriterTest.java
@@ -1,7 +1,10 @@
 package com.marklogic.spring.batch.item.writer;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.ext.DatabaseClientConfig;
 import com.marklogic.client.io.StringHandle;
@@ -15,11 +18,14 @@ import org.springframework.test.context.ContextConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 @ContextConfiguration(classes = {com.marklogic.spring.batch.config.MarkLogicConfiguration.class})
 public class MarkLogicPatchItemWriterTest extends AbstractSpringBatchTest {
 
     DatabaseClient client;
-    XMLDocumentManager docMgr;
+    XMLDocumentManager docMgrXml;
+    JSONDocumentManager docMgrJson;
 
     @Autowired
     DatabaseClientConfig batchDatabaseClientConfig;
@@ -31,23 +37,61 @@ public class MarkLogicPatchItemWriterTest extends AbstractSpringBatchTest {
                         batchDatabaseClientConfig.getUsername(), batchDatabaseClientConfig.getPassword());
         client = DatabaseClientFactory.newClient(batchDatabaseClientConfig.getHost(),
                 batchDatabaseClientConfig.getPort(), securityContext);
-        docMgr = client.newXMLDocumentManager();
+        docMgrXml = client.newXMLDocumentManager();
         StringHandle text1 = new StringHandle("<doc><text>Abbey D'Agostino finished the race Tuesday after helping Nikki Hamblin of New Zealand back up and urging her to finish. The two clipped heels during the late part of the race and tumbled to the ground. Hamblin has indicated she will run in the final.  Emma Coburn, who took bronze in the women's 3,000 steeplechase, becoming the first American woman to medal in the event, reacted Wednesday</text></doc>");
-        docMgr.write("hello.xml", text1);
+        docMgrXml.write("hello.xml", text1);
+
+        docMgrJson = client.newJSONDocumentManager();
+        StringHandle text2 = new StringHandle("{ \"a\": {\n" +
+                "    \"b\": \"value\",\n" +
+                "    \"c1\": 1,\n" +
+                "    \"c2\": 2,\n" +
+                "    \"d\": null,\n" +
+                "    \"e\": {\n" +
+                "      \"f\": true,\n" +
+                "      \"g\": [\"v1\", \"v2\", \"v3\"]\n" +
+                "    }\n" +
+                "} } ");
+        docMgrJson.write("hello.json",text2);
     }
+
 
     @Test
     public void patchItemWriterTest() throws Exception {
+
         MarkLogicPatchItemWriter itemWriter = new MarkLogicPatchItemWriter(client);
-        String[] info = new String[2];
-        info[0] = "hello.xml";
-        info[1] = "<new>content</new>";
-        List<String[]> strings = new ArrayList<String[]>();
-        strings.add(info);
-        itemWriter.write(strings);
-        StringHandle handle = docMgr.read("hello.xml", new StringHandle());
-        logger.info(handle.toString());
-        Fragment frag = new Fragment(handle.toString());
+
+        String[] infoJson = new String[2];
+        infoJson[0] = "hello.json";
+        infoJson[1] = "{ \"new\": \"content\"}";
+        List<String[]> stringsJson = new ArrayList<String[]>();
+        stringsJson.add(infoJson);
+        itemWriter.write(stringsJson);
+        StringHandle handleJson = docMgrJson.read("hello.json", new StringHandle());
+        logger.info(handleJson.toString());
+
+
+        String[] infoXml = new String[2];
+        infoXml[0] = "hello.xml";
+        infoXml[1] = "<new>content</new>";
+        List<String[]> stringsXml = new ArrayList<String[]>();
+        stringsXml.add(infoXml);
+        itemWriter.write(stringsXml);
+        StringHandle handleXml = docMgrXml.read("hello.xml", new StringHandle());
+        logger.info(handleXml.toString());
+
+        Fragment frag = new Fragment(handleXml.toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode actualObj = mapper.readTree(handleJson.toString());
+
+        //assert Json
+        assertThat(actualObj.findValuesAsText("new").get(0).toString(), equalTo("content"));
+
+        //assert Xml
         frag.assertElementExists("Expecting new content", "//new[text() = 'content']");
+
+
     }
 }
